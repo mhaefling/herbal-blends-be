58c12460e4ce1682bd7dd6a0b98751e59419b76f
Matt Haefling <m.haefling@protonmail.com>
2025-03-10 15:24:58 -0600

    test: subscriptionteas_model for validations and associations

---
+++ spec/models/subscription_tea.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe SubscriptionTea, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:subscription) }
+    it { should validate_presence_of(:tea) }
+    it { should validate_presence_of(:customer) }
+    it { should validate_presence_of(:status) }
+  end
+
+  describe "associations" do
+    it { should belong_to :subscription }
+    it { should belong_to :tea }
+    it { should belong_to :customer }
+  end
+end
