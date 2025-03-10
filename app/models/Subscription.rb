c6a298e548f0c7f3a5a50194eb6b6471be0674d6
Matt Haefling <m.haefling@protonmail.com>
2025-03-10 15:19:50 -0600

    test: tea_model for validations and associations

--- app/models/Subscription.rb
+++ app/models/Subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   validates :title, :price, :status, :frequency, presence: true
-  validates :price, numericality: { only_float: true }
+  validates :price, numericality: { only_float: true, greater_than: 0 }
 
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,12 @@ require "rails_helper"
 
 RSpec.describe Customer, type: :model do
   describe "validations" do
+    subject { Customer.create!(first_name: "Jolly", last_name: "Green", email: "jollygreen@gmail.com", address: "1 Jolly Green Road") }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:address) }
+    it { should validate_uniqueness_of(:email) }
   end
 
   describe "associations" do
