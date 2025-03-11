701680be49e9cd95e2f4e7f9b3c1619cbc45f73d
Matt Haefling <m.haefling@protonmail.com>
2025-03-10 21:51:41 -0600

    test: customer_controller index_action - passing

---
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+require "pry"
+
+RSpec.describe "Customer Controller", type: :request do
+  before :each do
+    @sub1 = Subscription.create!(title: "Green Tea Lovers", price: 5.50, status: true, frequency: "monthly")
+    @sub2 = Subscription.create!(title: "White Tea Lovers", price: 7.50, status: true, frequency: "monthly")
+    @tea1 = Tea.create!(title: "Arizona Green Tea", description: "Plastic bottle tea", temp: 2, brew_time: 0)
+    @tea2 = Tea.create!(title: "Black Tea", description: "The rare kind", temp: 2, brew_time: 15)
+    @tea3 = Tea.create!(title: "Silver Tea", description: "The ultimate rarety!", temp: 20, brew_time: 10)
+    @test_customer = Customer.create!(first_name: "Jolly", last_name: "Green", email: "JollyGreen@gmail.com", address: "123 JollyGreen Street")
+    @test_customer2 = Customer.create!(first_name: "Big", last_name: "Red", email: "BigRed@gmail.com", address: "123 BigRed Street")
+    @test_customer.subscription_teas.create!(subscription_id: @sub2.id, tea_id: @tea1.id, customer_id: @test_customer.id, status: true)
+    @test_customer2.subscription_teas.create!(subscription_id: @sub2.id, tea_id: @tea1.id, customer_id: @test_customer2.id, status: false)
+    @test_customer2.subscription_teas.create!(subscription_id: @sub2.id, tea_id: @tea3.id, customer_id: @test_customer2.id, status: true)
+  end
+
+  describe "Index Action" do
+    it 'Returns all customer data' do
+      get "/api/v1/customers"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      all_customers = JSON.parse(response.body, symbolize_names: true)
+      expect(all_customers[:data]).to be_a Array
+      expect(all_customers[:data].count).to eq(2)
+
+      all_customers_data = all_customers[:data]
+      all_customers_data.map do |customer|
+        expect(customer[:id]).to be_a String
+        expect(customer[:type]).to be_a String
+        expect(customer[:type]).to eq("customer")
+        expect(customer[:attributes]).to be_a Hash
+        expect(customer[:attributes][:first_name]).to be_a String
+        expect(customer[:attributes][:last_name]).to be_a String
+        expect(customer[:attributes][:email]).to be_a String
+        expect(customer[:attributes][:address]).to be_a String
+      end
+    end
+  end
+end
