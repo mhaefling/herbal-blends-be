7c000243d01002b12e09f1492c53b72aaa7288ee
Matt Haefling <m.haefling@protonmail.com>
2025-03-12 18:57:43 -0600

    Small bug fix to change status on sub_teas when sub is deactivated

--- app/models/Subscription.rb
+++ app/models/Subscription.rb
@@ -6,5 +6,11 @@ class Subscription < ApplicationRecord
   has_many :teas, through: :subscription_teas
   has_many :customers, through: :subscription_teas
 
+  after_update :subscription_teas_status_update, if: -> { saved_change_to_status? && !status }
   
+  private
+
+  def subscription_teas_status_update
+    subscription_teas.update_all(status: false)
+  end
 end
